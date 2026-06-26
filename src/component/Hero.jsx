f95df992bac5341c37b28a1432464839bf3dd9bc
Neha Sharma <neha2sharma15@gmail.com>
2026-06-26 14:26:22 +0530

    created Hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -6,26 +6,6 @@ const Hero = () => {
       <div className="max-w-7xl mx-auto px-6 md:px-12 grid md:grid-cols-2 gap-12 items-center">
         {/* LEFT CONTENT */}
         <div className="space-y-6 animate-fadeIn">
-          <span className="relative inline-block px-6 py-2.5 rounded-full group">
-            <span className="absolute -inset-1 bg-linear-to-r from-purple-600 to-pink-600 rounded-full opacity-75 group-hover:opacity-100 blur badge-pulse"></span>
-
-            <span className="relative flex items-center gap-3 px-5 py-2.5 text-sm font-semibold rounded-full bg-linear-to-r from-gray-900 to-gray-800 text-white">
-              <span className="flex gap-1">
-                <span className="w-1.5 h-1.5 bg-purple-400 rounded-full badge-bounce"></span>
-                <span className="w-1.5 h-1.5 bg-pink-400 rounded-full badge-bounce badge-delay-1"></span>
-                <span className="w-1.5 h-1.5 bg-purple-400 rounded-full badge-bounce badge-delay-2"></span>
-              </span>
-              Welcome to my Portfolio
-              <svg className="w-4 h-4 group-hover:rotate-180 transition-transform duration-500">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 7l5 5m0 0l-5 5m5-5H6"
-                />
-              </svg>
-            </span>
-          </span>
 
           <h1 className="text-3xl sm:text-4xl md:text-6xl font-extrabold leading-tight">
             Hello!<br></br> I’m <span className="text-blue-500">Neha Sharma</span>
